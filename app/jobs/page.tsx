2bb247004e60464c302f4d903cb6151e5541a0d1
mojtabababiker <mojmohammad98@gmail.com>
2025-04-02 04:17:13 +0200

    Update: adding jobs page placeholder

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,8 +1,33 @@
-import React from "react";
-
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import { InView } from "react-intersection-observer";
+import BreadCrumb from "../components/breadcrumb/breadcrumb";
 
 export default function Jobs() {
+  const [breadcrumb] = useState({
+    self: "Join The Team",
+    ancestors: [{ title: "Home", link: "/" }],
+  });
   return (
-    <h3 className="main-title">Hello From Jobs Page</h3>
-  )
+    <main className="contact-us-main">
+      <BreadCrumb
+        self={breadcrumb.self}
+        ancestors={breadcrumb.ancestors}
+        classType="contact-us-bg"
+      />
+      <section className="container-fluid section jop-container">
+        <InView
+          as="p"
+          className="secondary-p"
+          threshold={1}
+          onChange={(inView, entry) =>
+            inView && entry.target.classList.add("slide-right")
+          }
+        >
+          No jobs available at the moment. Please check back later.
+        </InView>
+      </section>
+    </main>
+  );
 }
